30efb6a36a9bc0c892c349c521ee8674b2a6ac86
Ingrid Ferreira <90401515+Ingrid2110rj@users.noreply.github.com>
2022-05-16 07:21:52 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -19,7 +19,7 @@ Cypress.Commands.add('entrega', () => {
   cy.get('.PerfilColaborador_boxItens__hmtzC > :nth-child(1) > input').type('21555500')
   cy.get(':nth-child(3) > input', { timeout: 12000 }).type('206')
   cy.get(':nth-child(4) > input').type('casa 2')
-  cy.get(':nth-child(8) > input', { timeout: 12000 }).type('975612544');
+  cy.get(':nth-child(8) > input', { timeout: 12000 }).type('999999999');
   cy.get('[type="submit"]').click({ force: true });
   cy.get('.Toastify__toast-body > :nth-child(2)').should('be.visible').and('have.text', 'Dados alterados com sucesso');
 
@@ -41,7 +41,7 @@ Cypress.Commands.add('confirmaReserva', () => {
   cy.get('input#64.seats').should('be.checked');
   cy.get('[value="Confirmar Seleção"]').click();
   cy.get('#button-form-reservarmesa').click();
-  cy.get('#p-text').should('contain', 'Reserva concluída no nome de Ingrid Ferreira dos Santos para o(os) dia(as) selecionado(os), no setor Setor A da Unidade Blumenau.');
+  cy.get('#p-text').should('contain', 'Reserva concluída no nome de Val para o(os) dia(as) selecionado(os), no setor Setor A da Unidade B.');
   cy.get('#button-fechar').click();
 })
 
@@ -62,7 +62,7 @@ Cypress.Commands.add('editaReserva', () => {
   cy.get('input#64.seats').click({ force: true });
   cy.get('[value="Confirmar Seleção"]').click({ force: true });
   cy.get('#button-form-reservarmesa').click({ force: true });
-  cy.get('#p-text').should('contain', 'Reserva editada no nome de Ingrid Ferreira dos Santos no Setor A andar para o dia 3 May 2022 na Unidade Blumenau.');
+  cy.get('#p-text').should('contain', 'Reserva editada no nome de Val no Setor A andar para o dia 3 May 2022 na Unidade B.');
   cy.get('#button-fechar').click({ force: true });
   cy.get('tbody > tr > :nth-child(1)').should('contain', '3 May 2022');
   cy.get('#button_delete_consulta').click({ force: true });
